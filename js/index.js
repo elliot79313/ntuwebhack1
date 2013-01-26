aecdb9d65a188bb69b402b839c86c3aa6104f9aa
elliot-air <elliot79313@gmail.com>
2013-01-26 13:02:29 +0800

    version: 1.4

--- js/index.js
+++ js/index.js
@@ -7,17 +7,24 @@
 			case 37: // left
 					yAngle -= 90;
 					break;
-			case 38: // up
-					xAngle += 90;
-					break;
 			case 39: // right
 					yAngle += 90;
-					break;
-			case 40: // down
-					xAngle -= 90;
 					break;
 		  };
           $('#cube')[0].style.webkitTransform = "rotateX("+xAngle+"deg) rotateY("+yAngle+"deg)";
 	  });
+	  new Youtube();
   });
 })(jQuery);
+var Youtube = function(){
+	var init = function(){
+		var params = { allowScriptAccess: "always" };
+    	var atts = { id: "myytplayer" };
+    	swfobject.embedSWF("http://www.youtube.com/v/Q3WxhfO2O4E?enablejsapi=1&playerapiid=ytplayer&version=3",
+                       "ytapiplayer1", "448", "252", "8", null, null, params, atts);
+		
+	};
+	init();
+	var  queue = function(){
+	}
+}
