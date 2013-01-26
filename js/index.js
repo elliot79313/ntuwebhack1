e9eb5e34e01276d924ed76a7012188ac2266a0e5
elliot-air <elliot79313@gmail.com>
2013-01-26 11:31:11 +0800

    Version: 1.3

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // JavaScript Document
+(function($){
+  $(document).ready(function(){
+	  var xAngle = 0, yAngle = 0;
+	  $("body").bind("keydown", function(e){
+		  switch(e.keyCode){
+			case 37: // left
+					yAngle -= 90;
+					break;
+			case 38: // up
+					xAngle += 90;
+					break;
+			case 39: // right
+					yAngle += 90;
+					break;
+			case 40: // down
+					xAngle -= 90;
+					break;
+		  };
+          $('#cube')[0].style.webkitTransform = "rotateX("+xAngle+"deg) rotateY("+yAngle+"deg)";
+	  });
+  });
+})(jQuery);
